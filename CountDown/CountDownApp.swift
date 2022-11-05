76933d91f7317824f9c90fc6a49e3f5479504637
Klaus Fischer <klaus.fischer@eloware.com>
2022-11-05 19:42:36 +0100

    Initial Commit

---
+++ CountDown/CountDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownApp.swift
+//  CountDown
+//
+//  Created by Klaus Fischer on 05.11.22.
+//
+
+import SwiftUI
+
+@main
+struct CountDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
